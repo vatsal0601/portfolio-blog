c5bb7721e109330d16cf93945402c7930a53976d
vatsal0601 <62330868+vatsal0601@users.noreply.github.com>
2022-01-15 16:06:14 +0530

    updated blog/project UI

--- pages/blog/[id].jsx
+++ pages/blog/[id].jsx
@@ -57,18 +57,18 @@ const Blog = ({ blog }) => {
 							/>
 						</div>
 					</section>
-					<div className="xl:flex xl:justify-between gap-10">
-						<section
-							ref={headingRef}
-							className="prose lg:prose-lg prose-zinc prose-blue prose-img:rounded-md prose-pre:rounded-md prose-headings:scroll-m-20 lg:prose-headings:scroll-m-24 dark:prose-invert">
-							<Markdown content={content} />
-						</section>
+					<div className="xl:flex gap-28">
 						<aside className="hidden xl:block bg-blue-100 dark:bg-zinc-800 sticky top-28 w-96 h-full p-8 rounded-md">
 							<p className="text-sm text-blue-700 dark:text-blue-500 font-semibold">On this page</p>
 							<div className="prose prose-zinc prose-a:no-underline prose-a:font-normal first:prose-ul:pl-0 prose-ul:list-outside prose-ul:list-none prose-li:pl-0 dark:prose-invert">
 								<Markdown content={toc} />
 							</div>
 						</aside>
+						<section
+							ref={headingRef}
+							className="prose lg:prose-lg prose-zinc prose-blue prose-img:rounded-md prose-pre:rounded-md prose-headings:scroll-m-20 lg:prose-headings:scroll-m-24 dark:prose-invert">
+							<Markdown content={content} />
+						</section>
 					</div>
 				</article>
 			</main>
