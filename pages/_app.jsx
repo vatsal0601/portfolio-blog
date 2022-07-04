4011a8f80921c325463d768c96e734338cb406ce
vatsal0601 <62330868+vatsal0601@users.noreply.github.com>
2022-07-04 17:11:56 +0530

    added google-analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 import Navbar from "@components/Navbar";
 import Footer from "@components/Footer";
 import ScrollButton from "@components/BackToTop";
+import Script from "next/script";
 import "../styles/tailwind.css";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "@lib/apolloClient";
@@ -9,12 +10,32 @@ const App = ({ Component, pageProps }) => {
 	const apolloClient = useApollo(pageProps.initialApolloState);
 
 	return (
-		<ApolloProvider client={apolloClient}>
-			<Navbar />
-			<ScrollButton />
-			<Component {...pageProps} />
-			<Footer />
-		</ApolloProvider>
+		<>
+			<Script
+				strategy="lazyOnload"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+
+			<Script strategy="lazyOnload" id="1">
+				{
+					/* JavaScript */ `
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+					page_path: window.location.pathname,
+					});
+						`
+				}
+			</Script>
+
+			<ApolloProvider client={apolloClient}>
+				<Navbar />
+				<ScrollButton />
+				<Component {...pageProps} />
+				<Footer />
+			</ApolloProvider>
+		</>
 	);
 };
 
