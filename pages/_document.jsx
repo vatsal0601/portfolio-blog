ec257b00c010bdd7a8d0c07f5eb0ca31c432c212
vatsal0601 <62330868+vatsal0601@users.noreply.github.com>
2022-07-25 08:57:56 +0530

    added google-site-verification tag

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,7 +9,12 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en" className="scroll-smooth">
-				<Head />
+				<Head>
+					<meta
+						name="google-site-verification"
+						content="rT8n5Cu4FDaSWq9htrL2nQnL8P_DzVdrqjmrvRCC9jY"
+					/>
+				</Head>
 				<body className="relative min-h-screen pb-48 pt-24 subpixel-antialiased transition-colors duration-300 selection:bg-blue-100 dark:bg-zinc-900 dark:selection:bg-zinc-800 lg:pb-56 lg:pt-32">
 					<Main />
 					<NextScript />
